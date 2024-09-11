e32fc34b123ce76e2c17183e4756d7fc4c59e2a3
ginda <baginda.s.hidayat@astrapay.com>
2024-09-11 10:10:16 +0700

    ok

--- prisma/migrationsBak/20240807125846_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [CEO] on the enum `Pengguna_posisi` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `pengguna` MODIFY `posisi` ENUM('STAFF', 'LEADER', 'ASMAN', 'MANAGER', 'DIREKTUR', 'KOMISARIS', 'DIREKTUR_UTAMA') NOT NULL;
